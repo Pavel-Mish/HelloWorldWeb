ba7c31c2b140968fe5bf6a4acb9cd2680481702b
Pavel-Mish <66910355+Pavel-Mish@users.noreply.github.com>
2020-09-11 12:26:22 +0300

    Initial commit

---
+++ src/test/java/com/example/HelloWorldWeb/HelloWorldWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
